b0103ae8da79bd83abebd82c92aaf7ba90a9bbad
EH <kyllingen@hotmail.com>
2024-07-25 21:35:16 +0200

    Day1 project

--- Day1/main.py
+++ Day1/main.py
@@ -6,3 +6,8 @@ print("Hello World!\nHello World!")
 
 #Concatenation
 print("Hello " + "Angela")
+
+#input and variable use. Use input and add result to a variable
+name = input("What is your name? ")
+nameLen = len(name)
+print(name, nameLen)
---
+++ Day1/project.py
@@ -0,0 +1,9 @@
+#Welcome message
+print("Welcome to the Band Name Generator")
+
+#input city and pet names and store as variables
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+
+#print out the band name
+print("Your band name could be " + city + " " + pet)
