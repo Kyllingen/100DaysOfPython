e31a90ee4b014996ebb6f05b1bf2396c8ae5903a
EH <kyllingen@hotmail.com>
2024-07-28 10:25:41 +0200

    added validation check

--- Day4/rockPaperScissors.py
+++ Day4/rockPaperScissors.py
@@ -37,6 +37,9 @@ print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
 userChoice = int(input())
 
 # Logic
+if userChoice >= 3 or userChoice < 0:
+    print("You typed an invalid number, you lose!")
+    
 computerChoice = random.randint(0, 2)
 result = resultList[userChoice][computerChoice]
 
