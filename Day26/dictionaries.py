791c062c1a36ea605f81cd94f5503c44dc43dba3
EH <kyllingen@hotmail.com>
2024-11-20 20:48:23 +0100

    additional examples

--- Day26/dictionaries.py
+++ Day26/dictionaries.py
@@ -2,6 +2,7 @@
 # new_dict = {new_key:new_value for item in list}
 # new_dict = {new_key:new_value for (key, value) in dict.items()}
 import random
+import pandas
 
 
 names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
@@ -10,3 +11,16 @@ print(students_score)
 
 passed_students = {name:score for (name,score) in students_score.items() if score >= 60}
 print(passed_students)
+
+# loop through dataframes by rows
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+student_frame = pandas.DataFrame(student_dict)
+print(student_frame)
+
+for (index, row) in student_frame.iterrows():
+    print(row)
+    print(row.student)
