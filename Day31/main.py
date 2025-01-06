7e0e6c3ccf16529959cde958285b33deb9a192ae
EH <kyllingen@hotmail.com>
2025-01-06 14:30:40 +0100

    Day31 UI setup

--- Day31/main.py
+++ Day31/main.py
@@ -12,9 +12,9 @@ window.title("Flasg Cards")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 # Canvas
-canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 
-image_logo = PhotoImage(file="images/card_back.png")
+image_logo = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=image_logo)
 
 canvas.create_text((400, 150), text="text", font="Arial 40 italic", justify="center")
@@ -23,11 +23,11 @@ canvas.grid(column=0, columnspan=2, row=0)
 
 #Buttons
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, border=0)
 right_button.grid(column=1, row=1)
 
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, border=0)
 wrong_button.grid(column=0, row=1)
 
 
