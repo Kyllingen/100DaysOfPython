be541a2bcbfdf78dacb66c11e0bf2a2fa3cebc01
EH <kyllingen@hotmail.com>
2025-02-03 21:57:54 +0100

    quizzler wrapup

--- Day34/ui.py
+++ Day34/ui.py
@@ -28,19 +28,45 @@ class QuizInterface:
         
         #Buttons
         right_image = PhotoImage(file="images/true.png")
-        self.right_button = Button(image=right_image, highlightthickness=0, border=0, padx=20, pady=20)
+        self.right_button = Button(image=right_image, highlightthickness=0, border=0, padx=20, pady=20, command=self.answer_true)
         self.right_button.grid(column=0, row=2, padx=20)
 
         wrong_image = PhotoImage(file="images/false.png")
-        self.wrong_button = Button(image=wrong_image, highlightthickness=0, border=0, padx=20, pady=20)
+        self.wrong_button = Button(image=wrong_image, highlightthickness=0, border=0, padx=20, pady=20, command=self.answer_false)
         self.wrong_button.grid(column=1, row=2, padx= 20)
         
         self.get_next_question()
         
         self.window.mainloop()
         
-    def get_next_question(self):
+    def get_next_question(self):        
         '''gets next question'''
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question_text, text=q_text)
+        self.canvas.config(bg=WHITE)
+        
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        
+            self.score.config(text=f"Score: {self.quiz.score}")
+        else:
+            self.canvas.itemconfig(self.question_text, text="QUIZ FINISHED!")
+            self.right_button.config(state="disabled")
+            self.wrong_button.config(state="disabled")
+        
+    def answer_true(self):
+        '''Answer True'''
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
     
+    def answer_false(self):
+        '''Answer False'''
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+        
+    def give_feedback(self, is_right):
+        '''Gives feedback on answer being correct or not'''
+        self.window.after(1000, self.get_next_question)
+        if is_right:
+            self.canvas.config(bg="#22FF22")
+        else:
+            self.canvas.config(bg="#FF2222")
